4069eaf9472d7f55b3aeea9a522a190c2e979008
Alexandr <alexandr532@proton.me>
2022-08-07 07:48:32 +0200

    Added icon font and sidebar icons

--- src/components/app/side-bar/side-bar.component.tsx
+++ src/components/app/side-bar/side-bar.component.tsx
@@ -1,8 +1,44 @@
 import './side-bar.styles.scss';
+import { Link } from 'react-router-dom';
 
 export function AppSideBar() {
   return (
   <div className='app-side-bar'>
-    <h3>Breacher::AppSideBar</h3>
+    <Link to='/home'>
+      <i className='icon-database' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-cubes' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-cog-alt' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-floppy' />
+    </Link>
+    <hr />
+    <Link to='/home'>
+      <i className='icon-plus' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-pencil' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-wrench' />
+    </Link>
+    <hr />
+    <Link to='/home'>
+      <i className='icon-lock' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-arrows-cw' />
+    </Link>
+    <hr/>
+    <Link to='/home'>
+      <i className='icon-info' />
+    </Link>
+    <Link to='/home'>
+      <i className='icon-bug' />
+    </Link>
   </div>
 )};
