0a984721326cabd0bcc74870db3f13fd341ef334
Alexandr <alexandr532@proton.me>
2022-08-09 08:14:43 +0200

    Fixed react strict mode double socket connection

--- src/components/app/app.component.tsx
+++ src/components/app/app.component.tsx
@@ -1,31 +1,14 @@
 import './app.styles.scss';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { io, Socket } from 'socket.io-client';
 import { AppStatusBar, AppHeader, AppSidebar } from '../app';
 import { Home, Breaches, Collections, Documents } from '../../components'
 
 export function App() {
+
   useEffect(() => {
     console.log('App Started');
-    connect();
   }, []);
-
-  function connect() {
-    try {
-      const socket: Socket = io('http://127.0.0.1:3003');
-      console.log('App Socket', socket);
-      socket.on('connect', () => {
-        console.log('Connected with socket.id:', socket.id);
-        socket.emit('auth', socket.id);
-      });
-      socket.on('disconnect', () => {
-        console.log('Disconnected'); 
-      });
-    } catch(e) {
-      console.log(e)
-    } 
-  }
 
   return (
     <div className='app'>
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { io, Socket } from 'socket.io-client';
 import './index.scss';
 import { App } from './components';
 import reportWebVitals from './report-web-vitals';
@@ -8,6 +9,17 @@ import reportWebVitals from './report-web-vitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const socket: Socket = io('http://localhost:3003');
+
+socket.on('connect', (): void => {
+  console.log('Connected with socket.id:', socket.id);
+  socket.emit('auth', socket.id);
+  socket.on('disconnect', () => {
+    console.log('Disconnected'); 
+  });
+});
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
